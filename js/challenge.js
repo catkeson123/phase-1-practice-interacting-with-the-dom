252ebed109801a21a685f9c1e001719eea19bbbe
Kevin Duggan <kevinduggan95@gmail.com>
2023-02-02 09:42:45 -0600

    takeover

--- js/challenge.js
+++ js/challenge.js
@@ -1,4 +1,5 @@
 const counter = document.querySelector("#counter");
+const likes = document.querySelector(".likes");
 
 document.addEventListener("DOMContentLoaded", function () {
   const plus = document.querySelector("#plus");
@@ -12,4 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
   minus.addEventListener("click", function () {
     counter.textContent=parseInt(counter.textContent)-1
   });
+
+  const favorite = document.createElement("li");
+
+  favorite.textContent="liked@"+counter.textContent
+  likes.append(favorite)
+
 });
+
