fbfffdb83a0a22303b4152196276062585a442aa
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-02-02 10:25:20 -0500

    first commit

---
+++ js/challenge.js
@@ -0,0 +1,11 @@
+const counter = document.querySelector("#counter");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const plus = document.querySelector("#plus");
+
+  plus.addEventListener("click", function () {});
+
+  const minus = document.querySelector("#minus");
+
+  minus.addEventListener("click", function () {});
+});
