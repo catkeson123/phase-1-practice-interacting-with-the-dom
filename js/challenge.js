31c234b8a58a2723b462d171fafb4405d801c276
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-02-02 10:32:12 -0500

    changes

--- js/challenge.js
+++ js/challenge.js
@@ -3,9 +3,13 @@ const counter = document.querySelector("#counter");
 document.addEventListener("DOMContentLoaded", function () {
   const plus = document.querySelector("#plus");
 
-  plus.addEventListener("click", function () {});
+  plus.addEventListener("click", function () {
+    counter.textContent=parseInt(counter.textContent)+1
+  });
 
   const minus = document.querySelector("#minus");
 
-  minus.addEventListener("click", function () {});
+  minus.addEventListener("click", function () {
+    counter.textContent=parseInt(counter.textContent)-1
+  });
 });
