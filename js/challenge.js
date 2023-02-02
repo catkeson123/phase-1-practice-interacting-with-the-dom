ca75d601c8288ff26a286467432ffc6cae6fa87f
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-02-02 11:27:27 -0500

    comment commit

--- js/challenge.js
+++ js/challenge.js
@@ -5,19 +5,33 @@ document.addEventListener("DOMContentLoaded", function () {
   const plus = document.querySelector("#plus");
 
   plus.addEventListener("click", function () {
-    counter.textContent=parseInt(counter.textContent)+1
+    counter.textContent = parseInt(counter.textContent) + 1;
   });
 
   const minus = document.querySelector("#minus");
 
   minus.addEventListener("click", function () {
-    counter.textContent=parseInt(counter.textContent)-1
+    counter.textContent = parseInt(counter.textContent) - 1;
   });
 
-  const favorite = document.createElement("li");
+  const heart = document.querySelector("#heart");
 
-  favorite.textContent="liked@"+counter.textContent
-  likes.append(favorite)
+  heart.addEventListener("click", function () {
+    const favorite = document.createElement("li");
+    favorite.textContent = "liked @ " + counter.textContent;
+    likes.append(favorite);
+  });
 
+  const commentForm = document.querySelector("#comment-form");
+  const div1 = document.querySelector("#list");
+  const commentList = document.createElement("ul");
+  div1.append(commentList);
+
+  commentForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const comment = document.createElement("li");
+    comment.textContent = e.target.comment.value;
+    commentList.append(comment);
+    commentForm.reset();
+  });
 });
-
